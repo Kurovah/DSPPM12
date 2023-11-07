881646916b6e8183e1ec766144d4ddbb0a2f547f
Kurovah <45458052+Kurovah@users.noreply.github.com>
2023-11-07 16:55:52 +0000

    driving physics
    these may be too arcadey

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -1,29 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
     public Rigidbody rb;
-    public float moveForce, steeringSmoothness;
     public Transform playerTransform;
+
+    [Header("Max speed & turn angle")]
+    public float maxSpeed;
+    public float maxTurnSpeed;
+
+    [Header("Smoothing")]
+    public float turnSmoothing;
+    public float speedsmoothing;
+
+    float currentSpeed, targetSpeed, currentTurning, targetTurning;
+    public Vector3 moveDir;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        targetSpeed = Input.GetKey(KeyCode.W) ? maxSpeed : 0;
+        targetTurning = (Input.GetAxis("Horizontal") != 0) ? maxTurnSpeed * Input.GetAxis("Horizontal") : 0;
 
-        Steer(Input.GetAxis("Horizontal"));
-        if (Input.GetKey(KeyCode.W))
-        {
-            Accel();
-        }
 
-        
+        currentSpeed = Mathf.Lerp(currentSpeed, targetSpeed, speedsmoothing);
+        currentTurning = Mathf.Lerp(currentTurning, targetTurning, turnSmoothing);
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(playerTransform.forward * currentSpeed, ForceMode.Acceleration);
+        playerTransform.eulerAngles = Vector3.Lerp(playerTransform.eulerAngles, new Vector3(0, playerTransform.eulerAngles.y + currentTurning, 0), Time.deltaTime * 4);
     }
 
     private void LateUpdate()
@@ -31,14 +47,4 @@ public class PlayerScript : MonoBehaviour
         playerTransform.position = rb.position;
     }
 
-    void Accel()
-    {
-        rb.AddForce(playerTransform.forward * moveForce);
-        //rb.AddForce(playerTransform.forward * moveForce, ForceMode.Acceleration);
-    }
-
-    void Steer(float _Direction)
-    {
-        transform.forward = Vector3.Lerp(transform.forward, transform.right * _Direction, steeringSmoothness * Mathf.Abs(Mathf.Sign(rb.velocity.magnitude)) * Time.deltaTime);
-    }
 }
