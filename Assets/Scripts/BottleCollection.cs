477567d221f791df1c049ea9da7eaaaba1cfdb89
StaceyVlasyuk <stasiavlasyuk@gmail.com>
2023-12-04 15:55:38 +0100

    NPCs done, bottles distributed + UI for bottle collection
    Map ready for user testing

--- Assets/Scripts/BottleCollection.cs
+++ Assets/Scripts/BottleCollection.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Data;
+using TMPro;
 using UnityEngine;
 
 public class BottleCollection : MonoBehaviour
 {
    public AudioSource audio;
    public int Bottle = 0;
+   public TextMeshProUGUI bottleText;
 
     public void OnTriggerEnter(Collider other) 
    {
@@ -14,6 +16,7 @@ public class BottleCollection : MonoBehaviour
     if(other.gameObject.tag == "Bottle") 
     {
 Bottle++;
+bottleText.text = "Bottles: " + Bottle.ToString();
 Debug.Log(Bottle);
 audio.Play();
 Destroy(other.gameObject);
